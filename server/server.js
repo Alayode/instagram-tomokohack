614b3ac32f6287c0ce233d79f910202a430618e5
chris <ksamuel.chris@gmail.com>
2015-02-19 18:22:33 -0500

    Added login required Middleware

--- server/server.js
+++ server/server.js
@@ -25,6 +25,34 @@ var User = mongoose.model('User',new mongoose.Schema({
 
 mongoose.connect(config.db);
 
+
+
+/*
+ |--------------------------------------------------------------------------
+ | Login Required Middleware
+ |--------------------------------------------------------------------------
+ */
+function isAuthenticated(req, res, next) {
+    if (!(req.headers && req.headers.authorization)) {
+        return res.status(400).send({ message: 'You did not provide a JSON Web Token in the Authorization header.' });
+    }
+    var header = req.headers.authorization.split(' ');
+    var token = header[1];
+    var payload = jwt.decode(token, config.tokenSecret);
+    var now = moment().unix();
+    if (now > payload.exp) {
+        return res.status(401).send({ message: 'Token has expired.' });
+    }
+    User.findById(payload.sub, function(err, user) {
+        if (!user) {
+            return res.status(400).send({ message: 'User no longer exists.' });
+        }
+        req.user = user;
+        next();
+    })
+}
+/*
+
 function createToken(user){
   var payload = {
     exp: moment().add(14, 'days').unix(),
