a7afd03f93a68233a600f9c27c2690b0c67bb7e1
chris <ksamuel.chris@gmail.com>
2015-02-20 12:34:12 -0500

    created email and password routes with JWT generator

--- server/server.js
+++ server/server.js
@@ -13,18 +13,17 @@ var request = require('request');
 
 var config = require('./config');
 
-var User = mongoose.model('User',new mongoose.Schema({
-  instagramId: { type: String, index: true },
-  email :  { type: String, unique: true, lowercase: true },
-  password: { type: String, select: false },
-  username: String,
-  fullname: String,
-  picture: String,
-  accessToken: String
+var User = mongoose.model('User', new mongoose.Schema({
+    instagramId: {type: String, index: true},
+    email: {type: String, unique: true, lowercase: true},
+    password: {type: String, select: false},
+    username: String,
+    fullname: String,
+    picture: String,
+    accessToken: String
 }));
 
 mongoose.connect(config.db);
-
 
 
 /*
@@ -34,77 +33,104 @@ mongoose.connect(config.db);
  */
 function isAuthenticated(req, res, next) {
     if (!(req.headers && req.headers.authorization)) {
-        return res.status(400).send({ message: 'You did not provide a JSON Web Token in the Authorization header.' });
+        return res.status(400).send({message: 'You did not provide a JSON Web Token in the Authorization header.'});
     }
     var header = req.headers.authorization.split(' ');
     var token = header[1];
     var payload = jwt.decode(token, config.tokenSecret);
     var now = moment().unix();
     if (now > payload.exp) {
-        return res.status(401).send({ message: 'Token has expired.' });
+        return res.status(401).send({message: 'Token has expired.'});
     }
-    User.findById(payload.sub, function(err, user) {
+    User.findById(payload.sub, function (err, user) {
         if (!user) {
-            return res.status(400).send({ message: 'User no longer exists.' });
+            return res.status(400).send({message: 'User no longer exists.'});
         }
         req.user = user;
         next();
     })
 }
-/*
-
-function createToken(user){
-  var payload = {
-    exp: moment().add(14, 'days').unix(),
-    iat: moment().unix(),
-    sub: user_id
-  };
-  return jwt.encode(payload, config.tokenSecret);
-}
 
 
 /*
-jwt-simple methods:
-  an object that gets encoded and decoded is called JWT claims.
-  encode()
-  decode()
+ |--------------------------------------------------------------------------
+ | JWT Generator
+ |--------------------------------------------------------------------------
+ */
 
-*/
-
-
-function isAuthenticated(req, res, next) {
-  if (!(req.headers &amp;amp;amp;&amp;amp;amp; req.headers.authorization)) {
-    return res.status(400).send({ message: 'You did not provide a JSON Web Token in the Authorization header.' });
-  }
-
-  var header = req.headers.authorization.split(' ');
-  var token = header[1];
-  var payload = jwt.decode(token, config.tokenSecret);
-  var now = moment().unix();
-
-  if (now &amp;amp;gt; payload.exp) {
-    return res.status(401).send({ message: 'Token has expired.' });
-  }
-
-  User.findById(payload.sub, function(err, user) {
-    if (!user) {
-      return res.status(400).send({ message: 'User no longer exists.' });
-    }
-
-    req.user = user;
-    next();
-  })
+function createToken(user) {
+    var payload = {
+        exp: moment().add(14, 'days').unix(),
+        iat: moment().unix(),
+        sub: user_id
+    };
+    return jwt.encode(payload, config.tokenSecret);
 }
+
+
+
+/*
+ |--------------------------------------------------------------------------
+ | Sign up using email verification
+ |--------------------------------------------------------------------------
+ */
+
+    app.post('/auth/login', function(req,res){
+        User.findOne({ email:req.body.email }, '+password', function(err, user) {
+            if (!user){
+                return res.status(401).send({ message: {email: 'Incorrect email'} });
+            }
+        })
+    })
+
+
+
+
+
+
+/*
+ |--------------------------------------------------------------------------
+ |  Create Email and Password Account
+ |--------------------------------------------------------------------------
+ */
+
+app.post('/auth/signup', function(req, res) {
+    User.findOne({ email: req.body.email }, function(err, existingUser) {
+        if (existingUser) {
+            return res.status(409).send({ message: 'Email is already taken.' });
+        }
+        var user = new User({
+            email: req.body.email,
+            password: req.body.password
+        });
+        bcrypt.genSalt(10, function(err, salt) {
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                user.password = hash;
+                user.save(function() {
+                    var token = createToken(user);
+                    res.send({ token: token, user: user });
+                });
+            });
+        });
+    });
+})
+
+
+
+
 
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended : false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(app.get('port'), function(){
-  console.log('Dropping you a Express server ' + app.get('port'));
+app.listen(app.get('port'), function () {
+    console.log('Dropping you a Express server ' + app.get('port'));
 
-})
+});
+
+
+
